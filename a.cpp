ce944f1b7954cabce6bccabe7379d95e7145e371
renzl <renzl@mapbar.com>
2019-09-24 19:05:14 +0800

    step1

--- a.cpp
+++ a.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-void Add(int a, int b)
+int Add(int a, int b)
 {
 	return a + b;
 }
 
-void Del(int a, int b)
+int Del(int a, int b)
 {
 	return a - b;
+}
+
+int Div(int a, int b)
+{
+	if (0 == b) return;
+	
+	return a / b;
 }
